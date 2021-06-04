564e0ebc8b0a9da98f561e3cae9021efbd929ca7
meryhf28 <73252401+meryhf28@users.noreply.github.com>
2021-06-04 19:29:07 +0200

    Update runConversion.R

--- Mapeo y cuantificación/runConversion.R
+++ Mapeo y cuantificación/runConversion.R
@@ -1,6 +1,10 @@
+# Conversión de gff3 a gtf
+
 # Se carga la librería rtrackplayer
 library("rtracklayer")
+
 # Se importa el archivo solea_v4.1.gff3
 dataset <- import("solea_v4.1.gff3")
+
 # Se exporta el fichero solea_v4.1.gtf en formato gtf
 export(dataset, "solea_v4.1.gtf", "gtf")
